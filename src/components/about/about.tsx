64ca0465dca8303dd4df84ea8e24dee9ee7354a1
Basicbay <151770227+Basicbay@users.noreply.github.com>
2024-01-09 10:02:11 +0700

    09/01/24

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -19,7 +19,7 @@ function About() {
 
         <motion.div className=' curve md:col-span-2 card bg-white place-items-center justify-center p-5 dark:bg-neutral-900 shadow-md ' whileHover={{ scale: 1.02 }}>
         
-        <div className="custom-shape-divider-top-1704694396 card z-0  opacity-5 dark:opacity-20">
+        <div className="custom-shape-divider-top-1704694396 card z-0  opacity-5 dark:opacity-40">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
     </svg>
@@ -60,7 +60,7 @@ function About() {
 
         <motion.div className=' text-left hyphens-auto md:col-span-3 bg-white card p-9 dark:bg-neutral-900 shadow-md ' lang='de' whileHover={{ scale: 1.02 }}>
         
-        <div className="custom-shape-divider-top-1704694396 card z-0 opacity-5 dark:opacity-20">
+        <div className="custom-shape-divider-top-1704694396 card z-0 opacity-5 dark:opacity-40">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
     </svg>
